6a79338e1fbb1e8d98f90cd43857ab11ab1bb199
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-03-06 18:38:49 +0530

    Update Buru.java

--- 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Buru.java
+++ 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Buru.java
@@ -2,14 +2,44 @@ package me.karunarathne.ProgrammingQuestions ;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Buru {
+    private static ArrayList <Card> deck ;
+    private static Scanner scanner ;
 
     public static void main(String[] args) {
-        ArrayList<Card> deck = Card.createDeck() ;
+        play() ;
+    }
 
+    public static void play () {
+        try {
+            scanner = new Scanner(System.in);
+            deck = Card.createDeck();
+
+            while (true) {
+                if (!mainLoop()) break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        } finally {
+            scanner.close() ;
+        }
+    }
+
+    private static boolean mainLoop () throws Exception {
         Collections.shuffle (deck) ;
 
-        for (Card card: deck) System.out.println(card.toString());
+        System.out.println ("Select a card") ;
+        Card selectedCard = cardSelector () ;
+
+        System.out.print ("Would you like to play again (Y/N) > ") ;
+        return ! scanner.nextLine().equalsIgnoreCase("n");
+    }
+
+    private static Card cardSelector() {
+        System.out.println ("\n\tS - Spades ♠\t\tD - Diamonds ♢\n" +
+                            "\tC - Clubs ♣\t\tH - Hearts ♡") ;
+        System.out.print ("\tSelect Suite > ") ;
     }
 }

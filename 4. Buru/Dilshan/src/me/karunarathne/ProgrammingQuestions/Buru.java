f1a145d34ab8173d32e48a01daf108b6e3498b9c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-03-06 15:51:51 +0530

    Update Buru.java

--- 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Buru.java
+++ 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Buru.java
@@ -1,7 +1,5 @@
 package me.karunarathne.ProgrammingQuestions ;
 
-import java.util.* ;
-
 public class Buru {
 
     public static void main(String[] args) {
@@ -9,4 +7,39 @@ public class Buru {
         
         
     }
+}
+
+class Shuffler {
+    
+    // A Function to generate a random permutation of arr[]
+    static void randomize( int arr[], int n)
+    {
+        // Creating a object for Random class
+        Random r = new Random();
+          
+        // Start from the last element and swap one by one. We don't
+        // need to run for the first element that's why i > 0
+        for (int i = n-1; i > 0; i--) {
+              
+            // Pick a random index from 0 to i
+            int j = r.nextInt(i);
+              
+            // Swap arr[i] with the element at random index
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+ 
+        // Prints the random array
+        System.out.println(Arrays.toString(arr));
+    }
+      
+    // Driver Program to test above function
+    public static void main(String[] args)
+    {        
+         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+         int n = arr.length;
+         randomize (arr, n);
+    }
+}
 }

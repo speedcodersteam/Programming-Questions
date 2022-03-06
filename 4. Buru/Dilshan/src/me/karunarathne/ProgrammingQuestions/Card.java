3658d93a49025e8ad9eedb30539cd653ed4bd8d1
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-03-06 15:23:02 +0530

    Update Card.java

--- 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Card.java
+++ 4. Buru/Dilshan/src/me/karunarathne/ProgrammingQuestions/Card.java
@@ -2,26 +2,75 @@ package me.karunarathne.ProgrammingQuestions;
 
 public class Card {
     Suit suit = null ;
-    String rank = null ;
+    Rank rank = null ;
 
     public Card (Suit suit, Rank rank) {
 
-    }
+    } 
 
     public Card (Suit suit, String rank) {
         
-    }
+    } 
 
     @Override
     public String toString() {
-        return super.toString() ;
+        return generateString() ;
+    }
+
+    private String generateString () {
+        // return "┌─────────┐\n| " + getLabel () + "      |\n"  + 
+        //         """
+        //     |         |
+        //     |         |
+        //     |         |
+        //         """ +
+        //     "|       " + getLabel () + "|\n└─────────┘ ";
+
+        return """
+        ┌─────────┐
+        | ##      |
+        |         |
+        |    #    |
+        |         |
+        |      ## |
+        └─────────┘
+                """.replace("#", suit) ;
+    }
+
+    private String getLabel() {
+        if (rank == Rank.Ten) return "10" ;
+        return " " + rank.toString() ;
     }
 }
 
 enum Suit {
-    Clubs, Diamonds, Hearts, Spades 
+    Clubs ("♣"), Diamonds ("♢"), Hearts ("♡"), Spades ("♠") ;
+
+    String value ;
+
+    Rank(String value) {
+        this.value = value ;
+    }
+
+    @Override
+    public String toString () {
+        return value ;
+    }
 } 
 
 enum Rank {
-    Ace, King, Queen, Jack, Ten , Nine, Eight, Seven, Six, Five, Four, Three, Deuce
+    Ace ("A"), King ("K"), Queen ("Q"), Jack ("J"), 
+    Ten ("10") , Nine ("9"), Eight ("8"), Seven ("7"), 
+    Six ("6"), Five ("5"), Four ("4"), Three ("3"), Deuce ("2") ;
+
+    String value ;
+
+    Rank(String value) {
+        this.value = value ;
+    }
+
+    @Override
+    public String toString () {
+        return value ;
+    }
 } 

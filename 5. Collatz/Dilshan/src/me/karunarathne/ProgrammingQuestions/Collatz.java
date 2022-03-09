4bf38ae5130a9c256c8ddde5346a1a7159a64b20
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-03-09 15:58:17 +0530

    Update Collatz.java

--- 5. Collatz/Dilshan/src/me/karunarathne/ProgrammingQuestions/Collatz.java
+++ 5. Collatz/Dilshan/src/me/karunarathne/ProgrammingQuestions/Collatz.java
@@ -1,3 +1,7 @@
-public class Collatz {
+package me.karunarathne.ProgrammingQuestions ;
 
+public class Collatz {
+	public static void main (String[] args) {
+		System.out.println ("Collartz!") ;
+	}
 }

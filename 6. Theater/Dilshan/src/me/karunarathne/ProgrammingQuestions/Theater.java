33ef8b8ff162b31edcd10473c276d74c19eab7d0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-03-18 21:33:48 +0530

    Fixes

---
+++ 6. Theater/Dilshan/src/me/karunarathne/ProgrammingQuestions/Theater.java
@@ -0,0 +1,9 @@
+package me.karunarathne.ProgrammingQuestions ;
+
+public class Theater {
+    int numberOfAttendees ;
+
+    private static calculateCost () {
+        return 180.0 + (0.04 * numberOfAttendees) ;
+    }
+}
